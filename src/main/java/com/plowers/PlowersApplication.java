725d8d62a6d4e33e741c514e73f055b0d48a6559
eun61n00 <eun61n@gmail.com>
2024-09-18 20:40:27 +0900

    Initial commit

---
+++ src/main/java/com/plowers/PlowersApplication.java
@@ -0,0 +1,13 @@
+package com.plowers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlowersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlowersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/plowers/PlowersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plowers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlowersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
