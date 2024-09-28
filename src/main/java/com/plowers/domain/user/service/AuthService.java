87a06982a1b93953b9c5a6b313801df8b941d5d9
eun61n00 <eun61n@gmail.com>
2024-09-28 17:49:31 +0900

    ✨FEAT: Validate Signup Request
    > Related to #4

--- src/main/java/com/plowers/domain/user/service/AuthService.java
+++ src/main/java/com/plowers/domain/user/service/AuthService.java
@@ -4,6 +4,7 @@ import com.plowers.domain.user.dto.request.SignUpRequest;
 import com.plowers.domain.user.dto.response.UserResponse;
 import com.plowers.domain.user.entity.User;
 import com.plowers.domain.user.repository.UserRepository;
+import com.plowers.domain.util.UserUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,10 +14,11 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final UserUtil userUtil;
     private final PasswordEncoder passwordEncoder;
 
-
     public UserResponse signup(SignUpRequest signUpRequest) {
+        validateRequest(signUpRequest);
         User user = new User(
                 signUpRequest.getUsername(),
                 passwordEncoder.encode(signUpRequest.getPassword()),
@@ -24,5 +26,13 @@ public class AuthService {
         );
         return UserResponse.of(userRepository.save(user));
     }
+
+    private void validateRequest(SignUpRequest signUpRequest) {
+        userUtil.isUsernameDuplicate(signUpRequest.getUsername());
+        userUtil.isEmailDuplicate(signUpRequest.getEmail());
+        userUtil.validateEmail(signUpRequest.getEmail());
+        userUtil.validatePassword(signUpRequest.getPassword());
+    }
+
 
 }
