9886b77983b1be2b1f87408311914d7671466ea5
eun61n00 <eun61n@gmail.com>
2024-10-01 11:29:53 +0900

    🐛FIX: typo

--- src/main/java/com/plowers/global/exception/ErrorMap.java
+++ src/main/java/com/plowers/global/exception/ErrorMap.java
@@ -10,7 +10,7 @@ public enum ErrorMap {
     INVALID_EMAIL_PATTERN(HttpStatus.BAD_REQUEST, 1000, "INVALID_EMAIL_PATTERN", "잘못된 이메일 형식입니다."),
     INVALID_PASSWORD_PATTERN(HttpStatus.BAD_REQUEST, 1001, "INVALID_PASSWORD_PATTERN", "비밀번호는 영문 소문자, 영문 대문자, 숫자, 특수문자가 각각 1개 이상 포함되어야 합니다."),
     PASSWORD_TOO_SHORT(HttpStatus.BAD_REQUEST, 1002, "PASSWORD_TOO_SHORT", "비밀번호는 8자리 이상이어야합니다."),
-    USER_ALREADY_EXISTS(HttpStatus.CONFLICT, 1003, "USER_ALREADY_EXISTS_ERROR", "이미 가입된 사용자입니다."),
+    USER_ALREADY_EXISTS(HttpStatus.CONFLICT, 1003, "USER_ALREADY_EXISTS", "이미 가입된 사용자입니다."),
     DUPLICATED_USERNAME(HttpStatus.CONFLICT, 1004, "DUPLICATED_USERNAME", "이미 사용중인 이름입니다."),
 
     // User(2xxx)
